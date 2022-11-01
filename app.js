f0b0cceb9bf5af68c3100a327f609974ceaa4aca
MatiPechu <matiasrobertone87@gmail.com>
2022-11-02 00:23:39 +0100

    ML hasta 01/11

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const express = require("express")
 const path = require ("path")
 const app = express();
 
-let Puerto = 3030;
+/*let PORT = 3030;*/
+let PORT= process.env.PORT ||3030
+app.listen(PORT,() => console.log("Listen on port " + PORT));
 
-app.listen(Puerto,() => console.log("Listen on port " + Puerto));
 
-app.use(express.static('public'));
 
 app.get("/",(req, res) => {
 res.sendFile(path.resolve("./views/home.html"))    
 })
+app.get("/login",(req, res) => {
+res.sendFile(path.resolve("./views/login.html"))    
+})
+app.get("/register",(req, res) => {
+res.sendFile(path.resolve("./views/register.html"))    
+})
+
+app.use(express.static('public'));
