c352be8a480860ae093ccac12e368d0220b0d475
MatiPechu <matiasrobertone87@gmail.com>
2022-11-15 12:44:07 -0300

    Cambio a estructura mvc

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 const express = require("express")
 const path = require ("path")
 const app = express();
+const mainRouters = require("./routers/mainRouters")
 
 /*let PORT = 3030;*/
 let PORT= process.env.PORT ||3030
 app.listen(PORT,() => console.log("Listen on port " + PORT));
 
 
-
-app.get("/",(req, res) => {
-res.sendFile(path.resolve("./views/home.html"))    
-})
-app.get("/login",(req, res) => {
-res.sendFile(path.resolve("./views/login.html"))    
-})
-app.get("/register",(req, res) => {
-res.sendFile(path.resolve("./views/register.html"))    
-})
-
 app.use(express.static('public'));
+
+app.use("/",mainRouters)
